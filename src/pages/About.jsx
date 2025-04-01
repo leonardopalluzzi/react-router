524f3621ae611de5730301f13e5aa4feb04e8b09
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-01 18:38:00 +0200

    Add About page with team section and background styling; enhance layout and content structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,41 @@
 export default function About() {
     return (
         <>
-            <h1>about</h1>
+            <section className="about">
+                <div className="cont_about">
+                    <div className="container py-5">
+                        <div className="content">
+                            <h1 className="py-3">About Us</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis reprehenderit necessitatibus fuga quibusdam voluptates sunt esse facere, perspiciatis dolore iste et natus obcaecati, quam nostrum aliquid laborum nobis commodi, praesentium explicabo? Magnam provident possimus voluptate neque explicabo non totam, a natus accusantium dolorum. Doloremque similique dolorem numquam explicabo aspernatur accusamus ad nisi earum debitis iste architecto reprehenderit eaque quaerat maxime nemo expedita quas, aperiam quia pariatur distinctio! Nemo neque nobis reiciendis culpa accusamus saepe distinctio perferendis doloribus, sunt, similique animi laboriosam tempora ea aspernatur, deleniti totam? Itaque doloribus illo omnis veniam. Ullam deleniti, at enim similique facilis a soluta!</p>
+                        </div>
+
+                        <div className="team py-5">
+                            <h2 className="py-4">Meet our Team</h2>
+                            <div className="row">
+                                <div className="col-3">
+                                    <img className="team_img" src="https://picsum.photos/200/300" alt="" />
+                                    <h3>Nome</h3>
+                                </div>
+                                <div className="col-3">
+                                    <img className="team_img" src="https://picsum.photos/200/300" alt="" />
+                                    <h3>Nome</h3>
+                                </div>
+                                <div className="col-3">
+                                    <img className="team_img" src="https://picsum.photos/200/300" alt="" />
+                                    <h3>Nome</h3>
+                                </div>
+                                <div className="col-3">
+                                    <img className="team_img" src="https://picsum.photos/200/300" alt="" />
+                                    <h3>Nome</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+
         </>
     )
 }
